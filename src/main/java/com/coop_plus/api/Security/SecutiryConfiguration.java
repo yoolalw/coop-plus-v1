134ff926724c1c322dd2998cc99d1168a5fb9480
yoolalw <larakemilyprofissional@gmail.com>
2026-06-22 16:51:48 -0300

    feat: SecurityConfiguration + @Bean SecurityFilterChain

---
+++ src/main/java/com/coop_plus/api/Security/SecutiryConfiguration.java
@@ -0,0 +1,25 @@
+package com.coop_plus.api.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecutiryConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.DELETE, "/admin").hasRole("admin")
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+}
